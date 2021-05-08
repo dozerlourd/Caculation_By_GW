bfe036ac727b2afbd3b58ca743c584f51626a7cf
유현창 <uu0506uu@gmail.com>
2021-05-09 02:58:46 +0900

    05/09 - trivial thing
    사소한 것 하나 수정 && 내일 할 것 주석 하나 달아놓음

--- Caculation_System/Assets/00.MyAsset/01.Script/Button/Operator/OperatorButtonScript.cs
+++ Caculation_System/Assets/00.MyAsset/01.Script/Button/Operator/OperatorButtonScript.cs
@@ -13,21 +13,27 @@ public class OperatorButtonScript : MonoBehaviour
 
     public void OnClickOperatorButton(string op)
     {
+        if(op == ")")
+        {
+            // 앞 (의 개수를 파악한 후 (보다 )가 더 많을 경우 리턴함
+        }
         calcSystem.Calc = op;
     }
 
     public void OnClickResultButton()
     {
-
+        
     }
 
     public void OnClickClearButton()
     {
-
+        calcSystem.NumberList.Clear();
+        calcSystem.RefreshCalculateUI();
     }
 
     public void OnClickBackspaceButton()
     {
-
+        calcSystem.NumberList.RemoveAt(calcSystem.NumberList.Count - 1);
+        calcSystem.RefreshCalculateUI();
     }
 }
